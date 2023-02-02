0e4bfdaec7ee6fc72989a87351d87df003ee4aab
Felix <93072175+felixbuch@users.noreply.github.com>
2023-02-02 20:01:58 +0100

    Update sendgrid.js
    Text

--- sendgrid.js
+++ sendgrid.js
@@ -12,14 +12,14 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     from: "hs-startup-crew@web.de", // Change to your verified sender
     subject: `"Hallo ${name} - Willkommen bei HS-Wismar-Talentmatching!"`,
     text: "Willkommen bei HS-Wismar-Talentmatching!",
-    html: `"<h1><strong>Hallo ${name} - Willkommen bei HS-Wismar-Talentmatching!</strong></h1>
+    html: `<h1><strong>Hallo ${name} - Willkommen bei HS-Wismar-Talentmatching!</strong></h1>
     <p>Vielen Dank für deine Anmeldung auf unserer Skillmatching Plattform.</p> <br><br>
     <p>Um dein Konto zu bestätigen, klicke bitte auf den folgenden Link:</p>
     <a href="https://felixbuch.github.io">Bestätigungslink</a><br><br>
     <p>Mit HS-Wismar-Talentmatching kannst du deine Fähigkeiten und Fertigkeiten mit anderen Studenten teilen, spannende Projekte starten und finden und vielleicht sogar ein studentisches Startup gründen.</p><br>
     <p>Wir freuen uns, dass du Teil unserer Community bist!</p><br>
     <p>Mit freundlichen Grüßen,</p><br>
-    <p>Das HS-Wismar-Talentmatching Team</p>"`,   
+    <p>Das HS-Wismar-Talentmatching Team</p>`,   
   };
   
 console.log("name is for recipient" + name)
