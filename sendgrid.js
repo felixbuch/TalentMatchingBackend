4f288f27a78f6a01340bd704c6417315e4180ec4
Felix <93072175+felixbuch@users.noreply.github.com>
2023-02-02 19:43:05 +0100

    Update sendgrid.js
    Anderer Email Text - Bestätigungslink führt nur zurück zur Website

--- sendgrid.js
+++ sendgrid.js
@@ -10,10 +10,18 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   let msg =  {
     to: `${email}`, // Change to your recipient
     from: "hs-startup-crew@web.de", // Change to your verified sender
-    subject: "Sending with SendGrid is Fun",
+    subject: `"Hallo ${name} - Willkommen bei HS-Wismar-Talentmatching!"`,
     text: "and easy to do anywhere, even with Node.js",
-    html: `"<strong>Hello ${name} this is an email for you</strong>"`,
+    html: `"<h1><strong>Hallo ${name} - Willkommen bei HS-Wismar-Talentmatching!</strong></h1>
+    <p>Vielen Dank für deine Anmeldung auf unserer Skillmatching Plattform.</p>
+    <p>Um dein Konto zu bestätigen, klicke bitte auf den folgenden Link:</p>
+    <a href="https://felixbuch.github.io">Bestätigungslink</a>
+    <p>Mit HS-Wismar-Talentmatching kannst du deine Fähigkeiten und Fertigkeiten mit anderen Studenten teilen, spannende Projekte starten und finden und vielleicht sogar ein studentisches Startup gründen.</p>
+    <p>Wir freuen uns, dass du Teil unserer Community bist!</p>
+    <p>Mit freundlichen Grüßen,</p>
+    <p>Das HS-Wismar-Talentmatching Team</p>"`,   
   };
+  
 console.log("name is for recipient" + name)
 console.log("email is for recipient" + email)
 console.log("msg is for recipient" + msg)
