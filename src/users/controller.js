6c720ae65c4acbb64f7317d72667f2d2c27c861e
Felix <desmonddachdekker@gmail.com>
2023-02-08 22:24:40 +0100

    fixed import of adduser function

--- src/users/controller.js
+++ src/users/controller.js
@@ -1,18 +1,21 @@
 const pool = require("../../db");
 const queries = require("./queries");
 const { assignSkillToUser } = require("../skills/queries");
-const sendMail = require("../../sendgrid");
+const {sendMail} = require("../../sendgrid");
 
 // addUser nutzt die FormData für das Submitten auf der Anmelden-Seite
 const addUser = async (req, res) => {
+  console.log("hello world")
   const { name, email, skills } = req.body;
-  //console.log(req.body);
+  console.log(req.body);
 
 
-  sendMail(name, email);
+ 
 
   results = await pool.query(queries.addUser, [name, email]);
   const user = results.rows[0]; // 0-tes Element des Arrays ist hier der User
+
+  sendMail(name, email);
 
   if (skills) {
     // Jeweils die Skill-IDs aus dem Array holen und mit User_ID in us_links-Tabelle schreiben
